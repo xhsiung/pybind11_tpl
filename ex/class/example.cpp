49e30a91d461179808db94690be9a771c8b07a0b
alex <alex@bais.com.tw>
2017-12-06 16:21:24 +0800

    add ex

---
+++ ex/class/example.cpp
@@ -0,0 +1,42 @@
+#include <pybind11/pybind11.h>
+#include <iostream>
+
+using namespace std;
+namespace py = pybind11;
+
+int add(int i, int j) {
+    return i + j;
+}
+
+//class不能用this oh my god!!
+class Pet{
+public:
+    string name;
+    int age;
+    Pet(){}
+    Pet(string name, int age):name(name), age(age){}
+    
+    string bark(string msg){
+        return msg;
+    }
+
+    int readnum(int a,int b){
+        return add(a,b);
+    }
+};
+
+
+PYBIND11_MODULE(example, m) {
+    py::class_<Pet>(m,"Pet")
+        .def(py::init<>())                 //default contruct
+        .def(py::init<string,int>())       //construct
+        .def_readwrite("name",&Pet::name)  //name property
+        .def_readwrite("age",&Pet::age)    //age  property
+        .def("bark" , &Pet::bark)          //func
+        .def("readnum" , &Pet::readnum);   //func
+}
+
+int main(int argc, char** argv){
+    cout << add(3,4) << endl;
+    return 0;
+}
---
+++ ex/class/test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+import example.my as my
+sum = my.add(3,4)
+print(sum)
