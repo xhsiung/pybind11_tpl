ed2d5af6a4503c4b30f389f6c99cfdb3478da210
alex <alex@bais.com.tw>
2017-12-15 16:43:25 +0800

    fix embed

--- ex/embed/main.cpp
+++ ex/embed/main.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 namespace py=pybind11;
 
-
 PYBIND11_EMBEDDED_MODULE(embed,m){
     m.def("add", [](int i, int j){
         return i+j ;
@@ -18,14 +17,29 @@ int main(){
     auto os = py::module::import("os");
     auto pwd = os.attr("getcwd")().cast<string>();
     
-    //sys.append( pwd )
+    //sys.append( mypath )
     auto sys = py::module::import("sys");
-    sys.attr("path").attr("append")( pwd );
-  
-    //embed
+    string mypath = pwd + "/tmp";
+    sys.attr("path").attr("append")( mypath );
+    py::print(sys.attr("path")  );
+
+    //add embed 
     auto embed = py::module::import("embed");
     auto result = embed.attr("add")(1,2).cast<int>();
     cout << result << endl;
+
+    //global local 
+    auto locals = py::dict();
+    locals["age3"] = 22;
+    py::exec( R"(
+        c=10
+        a=20
+        dict={'age':c,'age2':a}
+    )", py::globals(), locals );
+    //python type
+    py::print( locals["a"] );
+    //c++ type
+    cout << locals["age3"].cast<int>()  << endl;
 
     return 0;
 }
